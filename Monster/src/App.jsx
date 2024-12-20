79e417c1b47581e2837e4ba8aa64db82708588c3
siddique <siddiqueali531@gmail.com>
2024-12-20 17:41:41 +0500

    conflicts

--- Monster/src/App.jsx
+++ Monster/src/App.jsx
@@ -1,14 +1,10 @@
 
 import './App.css' 
 import HomeScreen from './Components/HomeScreen';
-import Slider from './Components/Slider';
-import SliderItem from './Components/SliderItem';
 function App() {
   return (
-  
    <>
-   {/* <HomeScreen/> */}
-   <Slider/>
+   <HomeScreen/>
    </>
     
   )
