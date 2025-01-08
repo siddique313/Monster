0c31fe25dce177d752ad72f62698c92464064398
siddique 313 <siddiqueali531@gmail.com>
2025-01-08 15:09:53 +0000

    sldjf

--- Monster/src/Components/HomeScreen.jsx
+++ Monster/src/Components/HomeScreen.jsx
@@ -9,6 +9,7 @@ import Pricing from "./Pricing";
 import Services from "./Services";
 import Team from "./Team";
 import Footer from "./Footer";
+import Slider from "./Slider";
 
 
 export default function HomeScreen() {
@@ -17,6 +18,7 @@ export default function HomeScreen() {
       <section className="relative ">
         <Header />
         <section className="px-20 flex flex-col gap-16">
+          <Slider/>
           <Services />
           <Develop />
           <Team />
